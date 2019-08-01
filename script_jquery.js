07c5b4ef5b922fd14c84f89cff458ca276e04bc6
Diego <diegooliveirafonseca18@gmail.com>
2019-08-01 17:56:37 -0300

    Segundo commit
    Fim do curso

--- script_jquery.js
+++ script_jquery.js
@@ -58,10 +58,87 @@ $(function(){
 */
 
 //Exercicio de delay - (espera a quantidade de microsegundos passados)
-
+/*
 $(function(){
 	$('#azul').click(function(){
 		$('p').fadeOut('slow');
 		$('p').delay(3000);
+		$('p').fadeIn('slow');
+	});
+});
+*/
+
+//Exercicio de Gravar Mensagem
+/*
+$('#azul').click(function(){
+	$('p').css("background-color","red");
+	$('#mensagem').text("Cor alterada com sucesso!");
+	$('#mensagem').css("color","red");
+	$('#mensagem').css("border","1px solid red");
+	$('#mensagem').delay(3000);
+	$('#mensagem').fadeOut();
+});
+*/
+
+//Exercicio de encadeamento em bloco
+//Evita a repetição da chamada do elemento
+/*
+$('#azul').click(function(){
+	$('p').css("background-color","red");
+	$('#mensagem')
+		.text("Cor alterada com sucesso!")
+			.css("color","red")
+				.css("border","1px solid red")
+					.delay(3000).fadeOut();
+});
+*/
+
+//Exercicio de declaração multipla de css
+//Declarações compostas devem usar CamelCase
+/*
+$('#azul').click(function(){
+	$('p').css("background-color","red");
+	$('#mensagem')
+		.text("Cor alterada com sucesso!")
+		.css({color:"red",border:"1px solid red",backgroundColor:'#F08080'})
+		.delay(3000)
+		.fadeOut();
+});
+*/
+
+//Exercicio Adicionando e removendo classes
+//No Css as propriedades inline tem prioridade sobre propriedade de arquivos e tag style
+//O ponto e virgula define o fim do comando. Como vemos a baixo o comando só termina após a remoção da class red do botão. Então serve para elementos diferentes na mesma instrução.
+/*
+$('#azul').click(function(){
+	$('p').css("background-color","red");
+	$('#mensagem')
+		.text("Cor alterada com sucesso!")
+		.css({color:"red",border:"1px solid red"})
+		.delay(3000)
+		.addClass('green')
+
+	$('button').removeClass('red');
+});
+*/
+
+//Exercicio de slideshow
+
+$(function(){
+	$('img').css({width:'300px',height:'300px'});
+	$('#l1').click(function(){
+		$('#i2').hide();
+		$('#i3').hide();
+		$('#i1').show();
+	});
+	$('#l2').click(function(){
+		$('#i1').hide();
+		$('#i3').hide();
+		$('#i2').show();
+	});
+	$('#l3').click(function(){
+		$('#i1').hide();
+		$('#i2').hide();
+		$('#i3').show();
 	});
 });
